96211ecd3d0f60b5802506a3de4150f122091319
Cyril ViXP <cyrilvixp@gmail.com>
2017-12-09 23:52:44 +0200

    Carrierwave, MiniMagick GEMs added, LogoUploader uploader is written, seeds edited

---
+++ app/uploaders/logo_uploader.rb
@@ -0,0 +1,49 @@
+class LogoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  attr_reader :height, :width
+
+  before :cache, :save_dimensions
+
+  storage :file
+
+  def store_dir
+    "uploads/teams/#{model.id}"
+  end
+
+  process resize_to_limit: [1000, 1000]
+
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
+
+  def content_type_whitelist
+    /image\//
+  end
+
+  def filename
+    'logo.jpg' if original_filename
+  end
+
+  def size_range
+    2_048...6_291_456
+  end
+
+  private
+
+  def save_dimensions file
+    return true unless version_name.blank?
+    if file.path.nil?
+      img = CarrierWave::MiniMagick::Image::read(file.file)
+      @width = img[:width]
+      @height = img[:height]
+    else
+      @width, @height = `identify -format "%wx %h" #{file.path}`.split(/x/)
+        .map{|dim| dim.to_i }
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+require 'open-uri'
+
+Captain.destroy_all
+cap1 = Captain.create!(
+  login: 'FirstCaptain', email: 'firstcaptain@mail.co',
+  password: 'SomeLongPassword123!',
+  salt: 'some_salt'
+)
+
+team1 = Team.create!(
+  captain: cap1,
+  title: 'First Captain team',
+  remote_logo_url: 'https://i.pinimg.com/originals/2d/cd/80/2dcd80c6f5a21a437'\
+    '313adde93b373d8.jpg'
+)
+
 100.times do |i|
-  Tournament.create(title: "Test tournament \##{i}")
+  Tournament.create!(title: "Test tournament \##{i}")
 end
